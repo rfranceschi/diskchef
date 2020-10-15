6f0c0c672c3f46596219defdbe943de307fcad64
Grigorii Smirnov-Pinchukov <smirnov@mpia.de>
2020-10-15 14:42:48 +0200

    Fix test with assert_allclose instead of arrays_almost_equal

--- diskchef/tests/test_chemistry/test_base.py
+++ diskchef/tests/test_chemistry/test_base.py
@@ -8,9 +8,10 @@ from diskchef.physics.parametrized import PowerLawPhysics
 
 def test_default_chemistry():
     chem = ChemistryBase(PowerLawPhysics())
-    numpy.testing.assert_almost_equal(chem.table["n(H+2H2)"][:10].value,
-                                      [3.74174141e+15, 2.95087606e+15, 2.32717032e+15, 1.83529284e+15,
-                                       1.44738001e+15, 1.14145756e+15, 9.00195773e+14, 7.09927777e+14,
-                                       5.59875378e+14, 4.41538491e+14])
+    numpy.testing.assert_allclose(chem.table["n(H+2H2)"][:10].value,
+                                  [3.7417414e+15, 2.9508761e+15, 2.3271703e+15, 1.8352928e+15,
+                                   1.4473800e+15, 1.1414576e+15, 9.0019577e+14, 7.0992778e+14,
+                                   5.5987538e+14, 4.4153849e+14],
+                                  )
     chem.run_chemistry()
-    numpy.testing.assert_almost_equal(chem.table["CO"][:10], 0.5e-4)
+    numpy.testing.assert_allclose(chem.table["CO"][:10], 0.5e-4)
