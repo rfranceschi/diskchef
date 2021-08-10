db8dc0382e66028ad82dba379d95353752a85ffd
Grigorii Smirnov-Pinchukov <smirnov@mpia.de>
2021-08-10 17:39:53 +0200

    Improve fitters

--- diskchef/fitting/fitters.py
+++ diskchef/fitting/fitters.py
@@ -1,10 +1,14 @@
 """Brut-force fitter for diskchef"""
+import logging
+
 import os
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from functools import partial
 from multiprocessing import Pool
+
+from matplotlib.figure import Figure
 from pathlib import Path
-from typing import Callable, List, Union, Literal
+from typing import Callable, List, Union, Literal, Dict
 from itertools import product, cycle, zip_longest
 
 import numpy as np
@@ -18,7 +22,8 @@ from emcee import EnsembleSampler
 from matplotlib.colors import LogNorm
 
 from diskchef.engine import CHEFNotImplementedError
-from diskchef.engine.overplot_scatter import overplot_scatter
+from diskchef.engine.exceptions import CHEFValueError
+from diskchef.engine.overplot_scatter import overplot_scatter, overplot_hexbin
 
 
 @dataclass
@@ -26,15 +31,62 @@ class Parameter:
     name: str
     min: Union[u.Quantity, float] = None
     max: Union[u.Quantity, float] = None
+    truth: float = None
+    format_: str = "{:.2f}"
+
+    def __post_init__(self):
+        self.fitted = None
+        self.fitted_error = None
+        self.fitted_error_up = None
+        self.fitted_error_down = None
+
+    @property
+    def math_repr(self):
+        out = "$"
+        if self.fitted is None:
+            out += self.name
+        elif self.fitted_error is None:
+            out += f"{self.name} = {self.format_.format(self.fitted)}"
+        elif (self.fitted_error_down is None) or (self.fitted_error_up is None):
+            out += f"{self.name} = {self.format_.format(self.fitted)} ± {self.format_.format(self.fitted_error)}"
+        else:
+            out += f"{self.name} = {self.format_.format(self.fitted)}" \
+                   f"^{{+{self.format_.format(self.fitted_error_up)}}}" \
+                   f"_{{-{self.format_.format(self.fitted_error_down)}}}"
+        if self.truth is not None:
+            out += f" ({self.format_.format(self.truth)})"
+        out += "$"
+        return out
+
+    def __eq__(self, other):
+        return self.fitted == other
 
 
 @dataclass
 class Fitter:
-    """Base class for finding parameters for lnprob which minimize its output"""
+    """Base class for finding parameters for lnprob which maximizes its output"""
     lnprob: Callable
     parameters: List[Parameter]
     threads: int = None
     progress: bool = False
+    hexbin: bool = True
+
+    def __post_init__(self):
+        self.logger = logging.getLogger(__name__ + '.' + self.__class__.__qualname__)
+        self.logger.info("Creating an instance of %s", self.__class__.__qualname__)
+        self.logger.debug("With parameters: %s", self.__dict__)
+        self._table = None
+        self.sampler = None
+        for parameter in self.parameters:
+            if any(
+                    fit_result is not None for fit_result in
+                    [parameter.fitted, parameter.fitted_error,
+                     parameter.fitted_error_up, parameter.fitted_error_down]
+            ):
+                self.logger.info("Parameter %s is already fit! Clearing the fitted values.", parameter)
+                parameter.fitted = parameter.fitted_error = None
+                parameter.fitted_error_up = parameter.fitted_error_down = None
+                self.logger.info("Parameter %s is cleaned.", parameter)
 
     def lnprob_fixed(self, *args, **kwargs):
         """Decorates self.lnprob so that minimum and maximum from self.parameters are considered"""
@@ -45,10 +97,6 @@ class Fitter:
                 return -np.inf
         return self.lnprob(*args, **kwargs)
 
-    def __post_init__(self):
-        self._table = None
-        self.sampler = None
-
     @property
     def table(self):
         return self._table
@@ -56,16 +104,44 @@ class Fitter:
     def fit(
             self,
             *args, **kwargs
-    ):
+    ) -> Dict[str, Parameter]:
         raise CHEFNotImplementedError
 
-    def corner(self, scatter_kwargs=None, **kwargs):
+    def corner(self, scatter_kwargs=None, hexbin_kwargs=None, **kwargs) -> Figure:
         if scatter_kwargs is None:
             scatter_kwargs = {}
+        if hexbin_kwargs is None:
+            hexbin_kwargs = {}
         data = np.array([self.table[param.name] for param in self.parameters]).T
-        fig = corner.corner(data, **kwargs)
-        overplot_scatter(fig, data, c=-self.table["lnprob"], norm=LogNorm(), **scatter_kwargs)
+        labels = [param.name for param in self.parameters]
+        truths = [parameter.truth for parameter in self.parameters]
+
+        if "weight" in self.table.colnames:
+            weights = self.table["weight"]
+            cumsumweights = np.cumsum(weights)
+            mask = cumsumweights > 1e-4
+        else:
+            weights = np.ones_like(data[:, 0])
+            mask = Ellipsis
+
+        fig = corner.corner(
+            data[mask], weights=weights[mask], labels=labels, show_titles=False, truths=truths, **kwargs
+        )
+        self._decorate_corner(fig)
+        # overplot_scatter(fig, data, c=-self.table["lnprob"], norm=LogNorm(), **scatter_kwargs)
+        if self.hexbin:
+            overplot_hexbin(fig, data[mask], C=-self.table["lnprob"][mask], norm=LogNorm(), **hexbin_kwargs)
         return fig
+
+    def _decorate_corner(self, fig: Figure):
+        ndim = len(self.parameters)
+        axes = np.array(fig.axes).reshape((ndim, ndim))
+        for ax, parameter in zip(axes.diagonal(), self.parameters):
+            ax.set_title(parameter.math_repr)
+
+    @property
+    def parameters_dict(self) -> Dict[str, Parameter]:
+        return {parameter.name: parameter for parameter in self.parameters}
 
 
 @dataclass
@@ -93,14 +169,21 @@ class BruteForceFitter(Fitter):
                 tbl["lnprob"] = pool.map(lnprob, tbl)
         else:
             tbl["lnprob"] = [self.lnprob_fixed(parameters, *args, **kwargs) for parameters in tbl]
+        tbl["weight"] = np.exp(tbl["lnprob"] - tbl["lnprob"].max())
         self._table = tbl
-        return tbl[np.argmax(tbl["lnprob"])]
+        argmax_row = tbl[np.argmax(tbl["lnprob"])]
+        for parameter in self.parameters:
+            parameter.fitted = argmax_row[parameter.name]
+
+        return self.parameters_dict
 
 
 @dataclass
 class EMCEEFitter(Fitter):
     nwalkers: int = 100
     nsteps: int = 100
+    burn_steps: int = 30
+    burn_strategy: Literal[None, 'best'] = None
 
     def fit(
             self,
@@ -117,14 +200,29 @@ class EMCEEFitter(Fitter):
 
         sampler = EnsembleSampler(self.nwalkers, len(self.parameters), self.lnprob_fixed, args=args, kwargs=kwargs,
                                   pool=pool)
-        sampler.run_mcmc(pos0, self.nsteps, progress=self.progress)
+        sampler.run_mcmc(pos0, self.burn_steps, progress=self.progress)
+        if self.burn_strategy is None:
+            pos1 = None
+        elif self.burn_strategy == 'best':
+            pos1 = np.tile(sampler.flatchain[np.argmax(sampler.flatlnprobability)], [self.nwalkers, 1])
+        else:
+            raise CHEFValueError("burn_strategy should be None or 'best'")
+        sampler.run_mcmc(pos1, self.nsteps - self.burn_steps, progress=self.progress)
         if pool is not None:
             pool.close()
         self.sampler = sampler
-        tbl = QTable(sampler.flatchain, names=[par.name for par in self.parameters])
-        tbl["lnprob"] = sampler.flatlnprobability
+        tbl = QTable(sampler.flatchain[self.burn_steps * self.nwalkers:], names=[par.name for par in self.parameters])
+        tbl["lnprob"] = sampler.flatlnprobability[self.burn_steps * self.nwalkers:]
         self._table = tbl
-        return tbl[np.argmax(tbl["lnprob"])]
+
+        for i, parameter in enumerate(self.parameters):
+            results = np.percentile(sampler.flatchain[self.burn_steps * self.nwalkers:, i], [16, 50, 84])
+            parameter.fitted = results[1]
+            parameter.fitted_error_up = results[2] - results[1]
+            parameter.fitted_error_down = results[1] - results[0]
+            parameter.fitted_error = (parameter.fitted_error_up + parameter.fitted_error_down) / 2
+
+        return self.parameters_dict
 
 
 @dataclass
@@ -132,25 +230,37 @@ class UltraNestFitter(Fitter):
     nwalkers: int = 100
     nsteps: int = 100
     transform: Callable = None
-    resume: Literal['resume', 'overwrite', 'subfolder'] = 'overwrite'
+    resume: Literal[True, 'resume', 'resume-similar', 'overwrite', 'subfolder'] = 'overwrite'
     log_dir: Union[str, Path] = None
+    run_kwargs: dict = field(default_factory=dict)
+
+    INFINITY = 1e50
+
+    def __post_init__(self):
+        super().__post_init__()
+        if self.transform is None:
+            self.transform = self.rescale
+
+    def rescale(self, cube):
+        params = np.empty_like(cube)
+        for i, parameter in enumerate(self.parameters):
+            params[i] = cube[i] * (parameter.max - parameter.min) + parameter.min
+        # params[0] = 10 ** (cube[0] * (np.log10(hi) - np.log10(lo)) + np.log10(lo))
+        return params
 
     def lnprob_fixed(self, *args, **kwargs):
-        return np.nan_to_num(super().lnprob_fixed(*args, **kwargs))
-
+        return np.nan_to_num(super().lnprob_fixed(*args, **kwargs),
+                             neginf=-self.INFINITY, posinf=self.INFINITY, nan=-self.INFINITY)
 
     def fit(
             self,
             *args, **kwargs
     ):
-        if self.threads != 1:
-            pool = Pool(self.threads)
-        else:
-            pool = None
         if self.log_dir is None:
             self.log_dir = Path("ultranest")
         else:
             self.log_dir = Path(self.log_dir)
+
         lnprob = partial(self.lnprob_fixed, *args, **kwargs)
         lnprob.__name__ = self.lnprob.__name__
         sampler = ultranest.ReactiveNestedSampler(
@@ -158,24 +268,25 @@ class UltraNestFitter(Fitter):
             lnprob,
             self.transform,
             log_dir=self.log_dir,
-            resume=True,
+            resume=self.resume,
             storage_backend='csv'
         )
-        if pool is None:
-            sampler.run()
-        if pool is not None:
-            pool.map(sampler.run, [None] * min([self.threads, os.cpu_count()]))
-            pool.close()
-        sampler.plot_run()
-        sampler.plot_trace()
-        sampler.plot_corner()
+        sampler.run(**self.run_kwargs)
+        sampler.plot()
         self.sampler = sampler
-        # tbl = QTable(sampler.flatchain, names=[par.name for par in self.parameters])
-        # tbl["lnprob"] = sampler.flatlnprobability
-        # self._table = tbl
-        # return tbl[np.argmax(tbl["lnprob"])]
-        return {
-            parname: parvalue
-            for parname, parvalue in
-            zip(sampler.paramnames, sampler.results['posterior']['mean'])
-        }
+
+        results = sampler.results['posterior']
+
+        tbl = QTable(sampler.results['weighted_samples']['points'], names=[par.name for par in self.parameters])
+        tbl["lnprob"] = sampler.results['weighted_samples']['logl']
+        tbl["lnprob"][tbl["lnprob"] <= -self.INFINITY] = -np.inf
+        tbl["weight"] = sampler.results['weighted_samples']['weights']
+        self._table = tbl
+
+        for i, parameter in enumerate(self.parameters):
+            parameter.fitted = results['mean'][i]
+            parameter.fitted_error_up = results['errup'][i] - results['mean'][i]
+            parameter.fitted_error_down = results['mean'][i] - results['errlo'][i]
+            parameter.fitted_error = results['stdev'][i]
+
+        return self.parameters_dict
--- diskchef/tests/fitting/test_linear_fitting.py
+++ diskchef/tests/fitting/test_linear_fitting.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List
 
 import numpy as np
@@ -39,6 +40,40 @@ def rescale_linear(cube):
     return params
 
 
+def sqr(
+        params: List[Parameter],
+        x: np.ndarray
+):
+    return params[0] * x ** 2 + params[1] * x + params[2]
+
+
+def sqr_model_lnprob(
+        params: List[Parameter],
+        x: np.ndarray,
+        y: np.ndarray,
+        weights: np.ndarray = None
+):
+    y_from_x = sqr(params, x)
+    if weights is None:
+        weights = np.ones_like(y)
+    chi2 = np.sum(weights ** 2 * (y - y_from_x) ** 2)
+    return -0.5 * chi2
+
+
+def rescale_sqr(cube):
+    params = np.empty_like(cube)
+    lo = 0.1
+    hi = 10
+    params[0] = 10 ** (cube[0] * (np.log10(hi) - np.log10(lo)) + np.log10(lo))
+    lo = -2
+    hi = 5
+    params[1] = cube[1] * (hi - lo) + lo
+    lo = -2
+    hi = 5
+    params[2] = cube[2] * (hi - lo) + lo
+    return params
+
+
 @pytest.mark.parametrize(
     "threads",
     [
@@ -50,10 +85,10 @@ def test_linear_brute(threads):
     y = linear([1, 2], x)
     assert linear_model_lnprob([1, 2], x, y) == 0
     a = Parameter(name="a", min=0.1, max=10)
-    b = Parameter(name="b", min=-2, max=2)
+    b = Parameter(name="b", min=-2, max=5)
     fitter = BruteForceFitter(lnprob=linear_model_lnprob, parameters=[a, b], n_points=100, threads=threads)
     bestfit = fitter.fit(x=x, y=y)
-    assert bestfit["a"], bestfit["b"] == pytest.approx([1, 2])
+    assert [bestfit["a"], bestfit["b"]] == pytest.approx([1, 2], rel=0.1, abs=0.5)
 
 
 @pytest.mark.parametrize(
@@ -67,18 +102,19 @@ def test_linear_emcee(threads):
     y = linear([1, 2], x)
     assert linear_model_lnprob([1, 2], x, y) == 0
     a = Parameter(name="a", min=0.1, max=10)
-    b = Parameter(name="b", min=-2, max=2)
+    b = Parameter(name="b", min=-2, max=5)
     fitter = EMCEEFitter(
         lnprob=linear_model_lnprob, parameters=[a, b],
-        # threads=threads
+        threads=threads
     )
     bestfit = fitter.fit(x=x, y=y)
-    assert bestfit["a"], bestfit["b"] == pytest.approx([1, 2])
+    assert [bestfit["a"], bestfit["b"]] == pytest.approx([1, 2], rel=0.1, abs=0.1)
+
 
 @pytest.mark.parametrize(
     "threads",
     [
-        1, 2,
+        1,
     ]
 )
 def test_linear_ultranest(threads):
@@ -86,30 +122,67 @@ def test_linear_ultranest(threads):
     y = linear([1, 2], x)
     assert linear_model_lnprob([1, 2], x, y) == 0
     a = Parameter(name="a", min=0.1, max=10)
-    b = Parameter(name="b", min=-2, max=2)
+    b = Parameter(name="b", min=-2, max=5)
     fitter = UltraNestFitter(
         lnprob=linear_model_lnprob, parameters=[a, b],
         transform=rescale_linear,
         threads=threads,
-        log_dir=f"test_{threads}"
+        log_dir=f"test_{threads}",
+        run_kwargs={"max_ncalls": 1e3}
     )
     bestfit = fitter.fit(x=x, y=y)
-    assert bestfit["a"], bestfit["b"] == pytest.approx([1, 2])
+    assert [bestfit["a"], bestfit["b"]] == pytest.approx([1, 2], rel=0.01, abs=0.1)
 
 
-if __name__ == '__main__':
-    threads = 1
+def example_lin():
     x = np.linspace(1, 10, 100)
     y = linear([1, 2], x)
 
-    a = Parameter(name="a", min=0.1, max=2)
-    b = Parameter(name="b", min=-2, max=5)
+    a = Parameter(name="a", min=0.1, max=2, truth=1)
+    b = Parameter(name="b", min=-2, max=5, truth=2)
     fitter = EMCEEFitter(lnprob=linear_model_lnprob, parameters=[a, b], threads=threads)
     bestfit = fitter.fit(x=x, y=y)
-    fitter.corner(truths=[1, 2])
+    fitter.corner()
 
     fitter = BruteForceFitter(lnprob=linear_model_lnprob, parameters=[a, b], threads=threads, n_points=100)
     bestfit = fitter.fit(x=x, y=y)
-    fitter.corner(truths=[1, 2])
+    fitter.corner()
+
+    fitter = UltraNestFitter(lnprob=linear_model_lnprob, parameters=[a, b], threads=threads, transform=rescale_linear)
+    bestfit = fitter.fit(x=x, y=y)
+    fitter.corner()
 
     plt.show()
+
+
+def example_sqr():
+    x = np.linspace(1, 10, 100)
+    y = sqr([1, 2, 0], x)
+
+    a = Parameter(name="a", min=0.1, max=2, truth=1)
+    b = Parameter(name="b", min=-2, max=5, truth=2)
+    c = Parameter(name="c", min=-2, max=5, truth=0)
+    params = [a, b, c]
+
+    fitter = EMCEEFitter(lnprob=sqr_model_lnprob, parameters=params, threads=threads)
+    bestfit = fitter.fit(x=x, y=y)
+    fitter.corner()
+
+    fitter = BruteForceFitter(lnprob=sqr_model_lnprob, parameters=params, threads=threads, n_points=40)
+    bestfit = fitter.fit(x=x, y=y)
+    fitter.corner()
+
+    fitter = UltraNestFitter(lnprob=sqr_model_lnprob, parameters=params, threads=threads,
+                             # transform=rescale_sqr
+                             )
+    bestfit = fitter.fit(x=x, y=y)
+    fitter.corner()
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    threads = 1
+    # example_lin()
+    example_sqr()
