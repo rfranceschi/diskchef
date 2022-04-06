bab292612d7ea4c6e765eb21a420eec82f9f0962
Lis Zwicky <foxastrophys@gmail.com>
2022-04-06 20:40:23 +0500

    Apply corrections to mol_weight function

--- diskchef/tests/chemistry/test_mol_weight.py
+++ diskchef/tests/chemistry/test_mol_weight.py
@@ -1,8 +1,27 @@
 from diskchef.engine import weights
 import pytest
 
-def test_weight():
-    assert int(weights.mol_weight('H2').value) == 2
-    assert int(weights.mol_weight('H2CO').value) == 30
-    assert int(weights.mol_weight('Hello').value) == 4 # hence we prove Hello weighs 4 u
-    assert int(weights.mol_weight('hello').value) == 0 # and that capitalisation creats mass in words
+
+@pytest.mark.parametrize(
+    "input, expected",
+    (
+            ("hello", 0),
+            ("Hello", 0),
+            ("FeSiM", 0),
+            ('H', 1),
+            ('D2O', 20),
+            ("H2CO", 30),
+            ("HCO+", 29),
+            ("gH2", 2),
+            ("gggH2", 0),
+            ("gagH2", 0),
+            ("aagH2", 0),
+            ("aaH2", 0),
+            ("aH3-", 3),
+            ("H++", 0),
+            ("H+-", 0),
+            ("H--", 0),
+    )
+)
+def test_weight(input, expected):
+    assert int(weights.mol_weight(input).value) == expected
