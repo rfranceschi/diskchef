21898a142292bb42883849038216553d1fbcf5e1
Lis Zwicky <foxastrophys@gmail.com>
2022-04-21 10:33:37 +0500

    Correct handling unsupported names

--- diskchef/tests/chemistry/test_mol_weight.py
+++ diskchef/tests/chemistry/test_mol_weight.py
@@ -5,23 +5,33 @@ import pytest
 @pytest.mark.parametrize(
     "input, expected",
     (
-            ("hello", 0),
-            ("Hello", 0),
-            ("FeSiM", 0),
             ('H', 1),
             ('D2O', 20),
             ("H2CO", 30),
             ("HCO+", 29),
             ("gH2", 2),
-            ("gggH2", 0),
-            ("gagH2", 0),
-            ("aagH2", 0),
-            ("aaH2", 0),
             ("aH3-", 3),
-            ("H++", 0),
-            ("H+-", 0),
-            ("H--", 0),
     )
 )
-def test_weight(input, expected):
+def test_weight_correct(input, expected):
     assert int(weights.mol_weight(input).value) == expected
+
+
+@pytest.mark.parametrize(
+    "inwrut",
+    (
+            ("hello"),
+            ("Hello"),
+            ("FeSiM"),
+            ("gggH2"),
+            ("gagH2"),
+            ("aagH2"),
+            ("aaH2"),
+            ("H++"),
+            ("H+-"),
+            ("H--"),
+    )
+)
+def test_weight_wrong(inwrut):
+    with pytest.raises(ValueError):
+        weights.mol_weight(inwrut)
